c608f33cbf6555834317ad8196e2de1a21ce056d
ldgeao99 <ldgeao99@gmail.com>
2019-02-08 11:38:56 +0900

    새로운 url규칙 추가 및 메소드 매칭

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from lotto import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls), #localhost:8000/admin일 때
+    url(r'^$', views.index),          #localhost:8000/일 때 views.index에 연결해준다.
 ]
