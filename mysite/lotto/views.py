3d04585585d1d12feb12cee249980ebe501d2593
ldgeao99 <ldgeao99@gmail.com>
2019-02-09 15:51:10 +0900

    view 로직 추가(index2)

--- mysite/lotto/views.py
+++ mysite/lotto/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
+def index(request): # request는 브라우저의 요청을 의미한다.
     return HttpResponse('<h1>Hello, Inflearn!</h1>')
+
+def index2(request):
+    return render(request, "lotto/default.html", {}) # 마지막 괄호에 오브젝프틑 포함시킬 수 있음
