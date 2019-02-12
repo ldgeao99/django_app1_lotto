4f90649de1d5e2b0838ee1984dbcd56038466efa
ldgeao99 <ldgeao99@gmail.com>
2019-02-12 09:50:51 +0900

    사용자 입력이 실제 DB에 반영되도록 post메소드 수정 localhost:8000/lotto/new 에서 아무거나 입력후 입력버튼을 눌러보면 'index' name을 가지는 localhost:8000/lotto로 리다이렉트 되어지며  localhost:8000/admin에서 확인하면 DB에 들어간 것도 볼 수 있다.

--- mysite/lotto/views.py
+++ mysite/lotto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -13,5 +13,12 @@ def index2(request):
     return render(request, "lotto/default.html", {"lottos" : lottos}) # 마지막 괄호에 있는건 딕셔너리 형태로 {}상태로 그냥 놔둬도 무관.
 
 def post(request):
-    form = PostForm()
-    return render(request, "lotto/form.html", {"form" : form})
+    if request.method == "POST":               # html 코드 에서 지정가능
+        form = PostForm(request.POST)
+        if form.is_valid():                    # form 객체의 속성을 사용자가 입력한 내용으로 교체한다.
+            lotto = form.save(commit = False)  # PostFrom 클래스 내부의 Meta클래스에서 명명한 GuessNumbers 객체가 생성됨.(내부의 데이터가 아직 채워지지 않아 아직 DB에 반영을 안시킴)
+            lotto.generate()                   # generate함수 내에서 DB 반영을 시킴
+            return redirect('index')           # urls에서 name속성에 맞는 쪽로 연결된다.
+    else:
+        form = PostForm()
+        return render(request, "lotto/form.html", {"form" : form})
