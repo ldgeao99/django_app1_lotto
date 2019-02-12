244279c3cf093b7ec117a548e989635ceb3be8ee
ldgeao99 <ldgeao99@gmail.com>
2019-02-12 09:10:30 +0900

    views.py에 post 메소드를 추가

--- mysite/lotto/views.py
+++ mysite/lotto/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 from .models import GuessNumbers
-
+from .form import PostForm
 
 def index(request): # request는 브라우저의 요청을 의미한다.
     return HttpResponse('<h1>Hello, Inflearn!</h1>')
@@ -11,3 +11,7 @@ def index(request): # request는 브라우저의 요청을 의미한다.
 def index2(request):
     lottos = GuessNumbers.objects.all()
     return render(request, "lotto/default.html", {"lottos" : lottos}) # 마지막 괄호에 있는건 딕셔너리 형태로 {}상태로 그냥 놔둬도 무관.
+
+def post(request):
+    form = PostForm()
+    return render(request, "lotto/form.html", {"form" : form})
